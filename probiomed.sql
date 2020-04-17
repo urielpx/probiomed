1f6da523f9a93133e0e84ac8b4667e2f376826c9
unknown <melissasoriano98@gmail.com>
2020-04-16 21:37:30 -0500

    se agrego la entidad cuenta clave y sus relaciones

--- probiomed.sql
+++ probiomed.sql
@@ -1,3 +1,12 @@
+CREATE TABLE cuenta_clave(
+	id 					int not null auto_increment unique,
+    nombre				varcharacter(100)	not null,
+    apellido_materno 	varcharacter(50),
+    apellido_paterno	varcharacter(50), 
+    edad 				int,
+    clave_trabajador	varcharacter(50)
+);
+
 CREATE TABLE doctor(
 	id 					int not null auto_increment unique,
     nombre				varcharacter(100)	not null,
@@ -16,9 +25,11 @@ CREATE TABLE farmacia(
 );
 
 CREATE TABLE mayorista(
-	id 				int not null auto_increment unique,
-    nombre			varcharacter(100)	not null,
-    razon_social 	varcharacter(200) not null
+	id 					int not null auto_increment unique,
+    nombre				varcharacter(100)	not null,
+    razon_social 	 	varcharacter(200) not null,
+    cuenta_clave_id 	int,
+    foreign key (cuenta_clave_id) references cuenta_clave(id)
 );
 
 CREATE TABLE producto(
@@ -28,5 +39,8 @@ CREATE TABLE producto(
     fecha_caducidad 	timestamp,
     peso				decimal,
     id_tipo_producto 	int,
-    foreign key (id_tipo_producto) references tipo_producto(id)
+    foreign key (id_tipo_producto) references tipo_producto(id),
+    cuenta_clave_id 	int,
+    foreign key (cuenta_clave_id) references cuenta_clave(id)
+    
 );
